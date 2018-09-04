e4af849553eb12be1c8240439572d6871c7479c8
Photon Cat <lkstc112233@gmail.com>
2018-09-04 15:54:16 -0400

    Added simple project.

---
+++ golomb compression/golomb compression/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  golomb compression
+//
+//  Created by Xu Ke on 2018/9/4.
+//  Copyright © 2018 Must Studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
