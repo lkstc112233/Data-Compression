31a6d5ddef5a6c54186545d6eca516079aca069e
Photon Cat <lkstc112233@gmail.com>
2018-09-04 19:49:09 -0400

    Implemented Diff

--- golomb compression/golomb encoder/main.cpp
+++ golomb compression/golomb encoder/main.cpp
@@ -51,10 +51,15 @@ int process(int count, int m, int lgm, bool mpb) {
 int main(int argc, const char * argv[]) {
     char c;
     std::vector<int> input;
+    std::vector<int> inputDiff;
     int zeroCount = 0, oneCount = 0;
+    int zeroCountDiff = 0, oneCountDiff = 0;
     char lastDigit = 0;
     int lastCount = 0;
+    char lastDigitDiff = 0;
+    int lastCountDiff = 0;
     bool firstBitIsZero = false;
+    bool firstBitIsZeroDiff = false;
     do {
         std::cin >> c;
         if (c != '0' && c != '1') {
@@ -76,14 +81,50 @@ int main(int argc, const char * argv[]) {
         if (c == '1') {
             oneCount += 1;
         }
+        
+        char lastDiffForConstruction;
+        if (lastDigitDiff == 0) {
+            firstBitIsZeroDiff = c == '0';
+            lastDiffForConstruction = lastDigitDiff = c;
+        } else {
+            if (lastDigit == c) {
+                lastDiffForConstruction = '0';
+            } else {
+                lastDiffForConstruction = '1';
+            }
+        }
+        if (lastDiffForConstruction == lastDigitDiff) {
+            lastCountDiff += 1;
+        } else {
+            inputDiff.push_back(lastCountDiff);
+            lastCountDiff = 1;
+        }
+        if (lastDiffForConstruction == '0') {
+            zeroCountDiff += 1;
+        }
+        if (lastDiffForConstruction == '1') {
+            oneCountDiff += 1;
+        }
+        if (lastDigit == c) {
+            lastDigitDiff = '0';
+        } else {
+            lastDigitDiff = '1';
+        }
         lastDigit = c;
     } while (c == '0' || c == '1');
     if (lastCount) {
         input.push_back(lastCount);
     }
+    if (lastCountDiff) {
+        inputDiff.push_back(lastCountDiff);
+    }
     bool useZero = false;
     if (zeroCount > oneCount) {
         useZero = true;
+    }
+    bool useZeroDiff = false;
+    if (zeroCountDiff > oneCountDiff) {
+        useZeroDiff = true;
     }
     std::cout << (useZero? '0': '1');
     double p = (useZero? zeroCount: oneCount) / double(zeroCount + oneCount);
@@ -102,6 +143,24 @@ int main(int argc, const char * argv[]) {
         careLast = care ? 1 : 0;
     }
     std::cout << (care? '0': '1') << std::endl;
-    std::cerr << "CR: " << (zeroCount + oneCount) << "/ " << codeLength << " = " << ((zeroCount + oneCount) / double(codeLength)) << std::endl;
+    std::cout << (useZeroDiff? '0': '1');
+    double pDiff = (useZeroDiff? zeroCountDiff: oneCountDiff) / double(zeroCountDiff + oneCountDiff);
+    double mupDiff = pDiff * log(2) / (1 - pDiff);
+    int mDiff = hob(int(mupDiff));
+    if (mDiff == 0) {
+        mDiff = 1;
+    }
+    int lgmDiff = log2(mDiff);
+    std::cerr << "p = " << pDiff << ", mup = " << mupDiff << ", m = " << mDiff << ", lgm = " << lgmDiff << std::endl;
+    bool careDiff = firstBitIsZeroDiff ^ useZeroDiff;
+    int careLastDiff = 0;
+    int codeLengthDiff = 2;
+    for (auto count : inputDiff) {
+        codeLengthDiff += process(count - careLastDiff, mDiff, lgmDiff, careDiff = !careDiff);
+        careLastDiff = careDiff ? 1 : 0;
+    }
+    std::cout << (careDiff? '0': '1') << std::endl;
+    std::cerr << "CR-No diff:\t" << (zeroCount + oneCount) << "/\t" << codeLength << "\t= " << ((zeroCount + oneCount) / double(codeLength)) << std::endl;
+    std::cerr << "CR-Diff:\t" << (zeroCountDiff + oneCountDiff) << "/\t" << codeLengthDiff << "\t= " << ((zeroCountDiff + oneCountDiff) / double(codeLengthDiff)) << std::endl;
     return 0;
 }
